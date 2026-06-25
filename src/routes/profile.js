e6b54ac4bb32fad15f10c0bf278ba5151f7bfadf
Utkarshcode1412 <pawarutkarsh579@gmail.com>
2026-06-25 21:28:29 +0530

    added password change functionality to profile route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth.js");
 const profileRouter = express.Router();
 const { validateProfileEdit } = require("../utils/validation.js");
+const bcrypt = require("bcrypt");
 
 
 
@@ -23,7 +24,7 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
 
         const loggedInUser = req.user;
 
-        Object.keys(req.body).forEach((keys) => (loggedInUser[keys] == req.body[keys]));
+        Object.keys(req.body).forEach((keys) => (loggedInUser[keys] = req.body[keys]));
 
         await loggedInUser.save();
 
@@ -35,6 +36,53 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
 
     } catch (err) {
         res.status(400).send("Error" + err.message);
+    }
+})
+
+
+profileRouter.patch("/profile/editPassword", userAuth, async(req, res) => {
+    try {
+        const { oldPassword, newPassword } = req.body;
+    
+        const user = req.user;
+        const isValid = await bcrypt.compare(
+            oldPassword,
+            user.password
+        );
+    
+        if(!isValid) {
+            return res.status(400).json({
+                success: false,
+                message: "Old password is incorrect"
+            });
+        }
+    
+        if(newPassword.length < 8) {
+            return res.status(400).json({
+                success: false,
+                message: "Password should conatin atleast 8 characters"
+            });
+        }
+    
+        if(newPassword === oldPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "New password cannot be same as old password"
+            });
+        }
+    
+        user.password = await bcrypt.hash(newPassword, 10);
+        await user.save();
+    
+        return res.status(200).json({
+            success: true,
+            message: "New password updated successfully"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        });    
     }
 })
 
